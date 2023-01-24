37d83da2f01a41df61a7f656794c900c85d684e7
Raihan <Raihan jishan@gmail.com>
2023-01-24 20:06:21 +0600

    upadte some thing else

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,9 +1,20 @@
+import { LocationCity, Share } from '@mui/icons-material';
 import React from 'react';
 import classes from "../styles/Mobilenav.module.css";
 const MobileNav = () => {
   return (
     <div className={classes.Container}>
-      <h1>Mobile Nav</h1>
+     <div className={classes.searchBox}>
+      <i className="fas fa-search"></i>
+     </div>
+     <div className={classes.Links}>
+      <a href="">Anywhere <i className="fas fa-city"></i></a>
+      <a href="">Any week <i className="fas fa-clock"></i></a>
+      <a href="">Add guest <i className="fas fa-person"></i></a>
+     </div>
+     <div className={classes.share}>
+          <Share />
+     </div>
     </div>
   )
 }
