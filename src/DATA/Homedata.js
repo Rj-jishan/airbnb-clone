bf8c936590aae1828c65b934b10a585d62d4cf15
Raihan <Raihan jishan@gmail.com>
2023-01-29 15:04:58 +0600

    devolopment right now!

--- src/DATA/Homedata.js
+++ src/DATA/Homedata.js
@@ -1,10 +1,59 @@
-const homePageData = [
-    {
-        id:1,
-        image :"",
-        location:"",
-        date:"",
-        price:"",
-        star:"",
-    }
-]
+// car image 
+// https://th.bing.com/th?id=OIP.K1Bol3dX7ETwwphDKoGzFwHaFg&w=289&h=215&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2
+ 
+export const homePageData = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1546587348-d12660c30c50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    location: "Emerald Lake - Yoho National Park",
+    date: "1 - 11 February",
+    price: "$31 night",
+    star: "***",
+  },
+  {
+    id: 2,
+    image:
+      "https://th.bing.com/th?id=OIP.K1Bol3dX7ETwwphDKoGzFwHaFg&w=289&h=215&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    location: "dw;ASDca;lsad",
+    date: "`1 - 10 dec",
+    price: "$100",
+    star: "***",
+  },
+  {
+    id: 3,
+    image:
+      "https://th.bing.com/th?id=OIP.K1Bol3dX7ETwwphDKoGzFwHaFg&w=289&h=215&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    location: "dw;ASDca;lsad",
+    date: "`1 - 10 dec",
+    price: "$100",
+    star: "***",
+  },
+  {
+    id: 4,
+    image:
+      "https://th.bing.com/th?id=OIP.K1Bol3dX7ETwwphDKoGzFwHaFg&w=289&h=215&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    location: "dw;ASDca;lsad",
+    date: "`1 - 10 dec",
+    price: "$100",
+    star: "***",
+  },
+  {
+    id: 5,
+    image:
+      "https://th.bing.com/th?id=OIP.K1Bol3dX7ETwwphDKoGzFwHaFg&w=289&h=215&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    location: "dw;ASDca;lsad",
+    date: "`1 - 10 dec",
+    price: "$100",
+    star: "***",
+  },
+  {
+    id: 6,
+    image:
+      "https://th.bing.com/th?id=OIP.K1Bol3dX7ETwwphDKoGzFwHaFg&w=289&h=215&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    location: "dw;ASDca;lsad",
+    date: "`1 - 10 dec",
+    price: "$100",
+    star: "***",
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import {homePageData as Homedata} from "../DATA/Homedata";
 import classes from "../styles/Home.module.css";
 const Home = () => {
   return (
@@ -7,93 +8,26 @@ const Home = () => {
       <main class={classes.main}>
       <div class={classes.container}>
         <div class={classes.videos}>
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+          {/*  map method  */} 
+          {Homedata.map((ref)=> {
+            return(
+              <>
+           
+        <div class={classes.video}>
+              <img src={ref.image} alt="404 error! " />
+              <p>{ref.location}</p>
               <div class={classes.qmeta}>
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
+                <p>{ref.price}</p>
+              <p>{ref.date}</p>
+                <p className={classes.review}>{ref.star}</p>
               </div>
             </div>
-          </a>
+        
 
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
-              </div>
-            </div>
-          </a>
+            </>
+            )
+          })}
 
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : 10 / 10</p>
-              </div>
-            </div>
-          </a>
-
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
-              </div>
-            </div>
-          </a>
-
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : 5 / 10</p>
-              </div>
-            </div>
-          </a>
-
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
-              </div>
-            </div>
-          </a>
-
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
-              </div>
-            </div>
-          </a>
-
-          <a href="quiz.html"
-            ><div class={classes.video}>
-              <img src="https://th.bing.com/th?id=OIP.uF1ZQujBaEWpXkTZDx0NPgHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
-              <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
-              <div class="qmeta">
-                <p>10 Questions</p>
-                <p>Score : Not taken yet</p>
-              </div>
-            </div>
-          </a>
         </div>
       </div>
     </main>
